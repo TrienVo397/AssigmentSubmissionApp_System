916fbfbc864091323c137e6d0842285ca86e9e64
TrienVo397 <s3907397@rmit.edu.vn>
2024-10-14 18:31:03 +0700

    adds all files

---
+++ src/main/java/com/Trien/AssigmentSubmissionApp/AssigmentSubmissionAppApplication.java
@@ -0,0 +1,13 @@
+package com.Trien.AssigmentSubmissionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssigmentSubmissionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssigmentSubmissionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Trien/AssigmentSubmissionApp/AssigmentSubmissionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Trien.AssigmentSubmissionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigmentSubmissionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
